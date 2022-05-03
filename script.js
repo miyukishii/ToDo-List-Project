0091742e055476af1e35c3cbb2f86d86fa9c1ba3
Miyuki Ishii <miyukisabrina@gmail.com>
2022-05-03 20:43:00 -0300

    Requisito 5 e 6 - Adicionando botão e ordenando tarefas

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const section = document.querySelector('#main-content');
+const board = document.querySelector('#main-content');
 
 // Criando list ordenada
 
 const list = document.createElement('ol');
 list.id = 'lista-tarefas';
-section.appendChild(list);
+board.appendChild(list);
+
+// Criar função do evento do botão adicionar tarefa
+
+function addTask() {
+    const input = document.querySelector('#texto-tarefa');
+    if(input.value !== '') {
+    const newTask = document.createElement('li');
+    newTask.innerHTML = input.value;
+    list.appendChild(newTask);
+    }
+    input.value = null;
+}
+
+// Criando evento no botão adicionar tarefa
+
+const btnAddTask = document.querySelector('#criar-tarefa');
+
+btnAddTask.addEventListener('click', addTask);
