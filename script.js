ccf64f325a3e4dd5e50b4ca730705a7321bf88f3
Miyuki Ishii <miyukisabrina@gmail.com>
2022-05-03 13:44:07 -0300

    Requisito 4 - adicionando lista ordenada

---
+++ script.js
@@ -0,0 +1,7 @@
+const section = document.querySelector('#main-content');
+
+// Criando list ordenada
+
+const list = document.createElement('ol');
+list.id = 'lista-tarefas';
+section.appendChild(list);
